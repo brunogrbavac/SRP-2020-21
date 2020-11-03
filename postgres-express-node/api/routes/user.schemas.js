2a687de63a32496a23cf572aa0db153c560192e5
brunogrbavac <bruno.grbavac@outlook.com>
2020-11-03 14:39:32 +0100

    Add initial login logic

--- postgres-express-node/api/routes/user.schemas.js
+++ postgres-express-node/api/routes/user.schemas.js
@@ -1,37 +1,37 @@
 exports.getUser = {
-  title: "Find user by a username",
-  type: "object",
-  properties: {
-    username: { type: "string" },
-  },
-  required: ["username"],
-};
-
-exports.createUser = {
-  title: "Add a new user",
-  type: "object",
-  properties: {
-    username: { type: "string", minLength: 1 },
-    password: { type: "string", minLength: 8 },
-  },
-  required: ["username", "password"],
-};
-
-exports.updateUser = {
-  title: "Update the existing user data",
-  type: "object",
-  properties: {
-    username: { type: "string", minLength: 1 },
-    password: { type: "string", minLength: 8 },
-  },
-  anyOf: [{ required: ["username"] }, { required: ["password"] }],
-};
-
-exports.deleteUser = {
-  title: "Delete an existing user",
-  type: "object",
-  properties: {
-    username: { type: "string", minLength: 1 },
-  },
-  required: ["username"],
-};
+    title: "Find user by a username",
+    type: "object",
+    properties: {
+      username: { type: "string" },
+    },
+    required: ["username"],
+  };
+  
+  exports.createUser = {
+    title: "Add a new user",
+    type: "object",
+    properties: {
+      username: { type: "string", minLength: 1 },
+      password: { type: "string", minLength: 8 },
+    },
+    required: ["username", "password"],
+  };
+  
+  exports.updateUser = {
+    title: "Update the existing user data",
+    type: "object",
+    properties: {
+      username: { type: "string", minLength: 1 },
+      password: { type: "string", minLength: 8 },
+    },
+    anyOf: [{ required: ["username"] }, { required: ["password"] }],
+  };
+  
+  exports.deleteUser = {
+    title: "Delete an existing user",
+    type: "object",
+    properties: {
+      username: { type: "string", minLength: 1 },
+    },
+    required: ["username"],
+  };
